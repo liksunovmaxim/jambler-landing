7b9a7c28d48d684d5c91095ff638e74f24dcb0b0
liksunovmaxim <liksunovmaxim@gmail.com>
2016-06-03 13:28:30 +0300

    update

--- frontend/javascripts/index.js
+++ frontend/javascripts/index.js
@@ -3,7 +3,15 @@ requirejs.config({
 });
 
 
-define(["jquery", "swiper"], function($) {
+define([
+	"jquery",
+	"swiper",
+	"ScrollMagic",
+	"velocity",
+	"animation.velocity",
+	"debug.addIndicators",
+	"jquery.ScrollMagic"
+], function($, a, ScrollMagic) {
 	"use strict";
 
 	var swiper = new Swiper('.swiper-container', {
@@ -13,4 +21,12 @@ define(["jquery", "swiper"], function($) {
 		scrollbarHide: false
 	});
 
+	var controller = new ScrollMagic.Controller();
+
+	// build scene
+	var scene = new ScrollMagic.Scene({triggerElement: "#trigger"})
+		// trigger a velocity opaticy animation
+		.setVelocity("#animate", {opacity: 0}, {duration: 400})
+		.addIndicators() // add indicators (requires plugin)
+		.addTo(controller);
 });
