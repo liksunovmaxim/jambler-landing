38291027e482e7db2e8120b365d7b6d806f7d603
liksunovmaxim <liksunovmaxim@gmail.com>
2016-06-03 14:44:37 +0300

    update

--- public/js/index.js
+++ public/js/index.js
@@ -7,10 +7,12 @@ define([
 	"jquery",
 	"swiper",
 	"ScrollMagic",
+	"TweenMax",
 	"velocity",
 	"animation.velocity",
+	"animation.gsap",
 	"debug.addIndicators",
-	"jquery.ScrollMagic"
+
 ], function($, a, ScrollMagic) {
 	"use strict";
 
@@ -23,10 +25,15 @@ define([
 
 	var controller = new ScrollMagic.Controller();
 
+	var tween1 = TweenMax.staggerFromTo(".phone__images-block", 2, {top: 700, opacity: 0}, {top: 0, opacity:1, ease: Back.linear}, 0.15);
+
 	// build scene
-	var scene = new ScrollMagic.Scene({triggerElement: "#trigger"})
-		// trigger a velocity opaticy animation
-		.setVelocity("#animate", {opacity: 0}, {duration: 400})
+	var scene = new ScrollMagic.Scene({triggerElement: "#trigger", duration: 300})
+		.setTween(tween1)
+		.addIndicators() // add indicators (requires plugin)
+		.addTo(controller);
+	var scene2 = new ScrollMagic.Scene({triggerElement: "#trigger", duration: 200, offset: 300})
+		.setPin(".phone__images-block img")
 		.addIndicators() // add indicators (requires plugin)
 		.addTo(controller);
 });
